2e7822eb6ec65898798e612d290107eaeaf4e1c2
Almira <jasinskyte.almira@gmail.com>
2022-08-23 14:19:49 +0300

    last push

--- react/src/components/organisms/shopAndServicemanList/ShopAndServicemenList.jsx
+++ react/src/components/organisms/shopAndServicemanList/ShopAndServicemenList.jsx
@@ -1,1 +1,29 @@
-import "./style.scss"
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import './style.scss'
+
+export const ServiceList = () => {
+
+    const [shops, setShops] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:3003/admin/shops')
+            .then(res => setShops(res.data))
+    }, [])
+
+    return (<div className="table">
+        <div className="headers">
+            <div>name</div>
+            <div>address</div>
+        </div>
+        <div className="table">
+            {shops.map(shop => (
+                <div className="listItem">
+                    <div>{shop.shop_name}</div>
+                    <div>{shop.address}</div>
+                </div>
+                )
+            )}
+        </div>
+    </div>)
+}
--- react/src/components/pages/shopAndServicemenListAdminPage/ShopAndServicemenListAdminPage.jsx
+++ react/src/components/pages/shopAndServicemenListAdminPage/ShopAndServicemenListAdminPage.jsx
@@ -1,9 +1,10 @@
+import { ServiceList } from "../../organisms/shopAndServicemanList/ShopAndServicemenList"
 import "./style.scss"
 
 export const ShopAndServicemenListAdminPage = () => {
     return (
         <div className="list-admin">
-            
+            <ServiceList />
         </div>
     )
 }
