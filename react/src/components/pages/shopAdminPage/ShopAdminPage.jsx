c85baf5e56448902a0162157922dc7438ca04436
Almira <jasinskyte.almira@gmail.com>
2022-08-23 11:18:40 +0300

    create shop form

--- react/src/components/pages/shopAdminPage/ShopAdminPage.jsx
+++ react/src/components/pages/shopAdminPage/ShopAdminPage.jsx
@@ -1,9 +1,10 @@
+import { ShopCreationForm } from "../../organisms/shopCreationForm/ShopCreationForm"
 import "./style.scss"
 
 export const ShopAdminPage = () => {
     return (
         <div className="shop-admin">
-            
+            <ShopCreationForm />
         </div>
     )
 }
--- react/src/components/pages/shopAndServicemenListAdminPage/ShopAndServicemenListAdminPage.jsx
+++ react/src/components/pages/shopAndServicemenListAdminPage/ShopAndServicemenListAdminPage.jsx
@@ -3,7 +3,7 @@ import "./style.scss"
 export const ShopAndServicemenListAdminPage = () => {
     return (
         <div className="list-admin">
-
+            
         </div>
     )
 }
