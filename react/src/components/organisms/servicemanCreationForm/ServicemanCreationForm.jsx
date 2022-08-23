c55ca926e09fe28409ed25b73528ea39523f98fe
Almira <jasinskyte.almira@gmail.com>
2022-08-23 11:31:34 +0300

    admin side creation forms

---
+++ react/src/components/organisms/servicemanCreationForm/ServicemanCreationForm.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { Button } from "../../atoms/button/Button";
+import { TextField } from "../../atoms/textField/TextField";
+import "./style.scss"
+
+export const ServicemanCreationForm = ({onCreate}) => {
+      
+    const [shopName, setShopName] = useState('');
+    const [shopAddress, setShopAddress] = useState('');
+
+    return (
+        <div className="serviceman-creation-form">
+            <TextField label="Enter Shop Name" name="shopName" value={shopName} onChange={e => setShopName(e.target.value)}/>
+            <TextField label="Enter shop address" name="shopAddress" value={shopAddress} onChange={e => setShopAddress(e.target.value)}/>
+            <Button variant="secondary" onClick={() => onCreate({shopName, shopAddress})}>Create Shop</Button>
+        </div>
+    );
+}
--- react/src/components/pages/servicemanAdminPage/ServicemanAdminPage.jsx
+++ react/src/components/pages/servicemanAdminPage/ServicemanAdminPage.jsx
@@ -1,9 +1,10 @@
+import { ServicemanCreationForm } from "../../organisms/servicemanCreationForm/ServicemanCreationForm"
 import "./style.scss"
 
 export const ServicemanAdminPage = () => {
     return (
         <div className="serviceman-admin">
-
+            <ServicemanCreationForm />
         </div>
     )
 }
